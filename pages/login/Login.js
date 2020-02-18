7ecd3cc162bf05062471fcc35054dda2622376f9
oksana <oksanana@wizardsdev.com>
2020-02-18 17:52:08 +0200

    create app

--- pages/login/Login.js
+++ pages/login/Login.js
@@ -28,52 +28,6 @@ const Login = () => {
                                         <Button type='submit'>Submit</Button>
                                     </Form>
                                 </Segment>
-                                {/*<div className="is-login__form-logo">*/}
-                                {/*    <Image src="/static/icons/logo_color.svg" className='is-login__form-logo-img'/>*/}
-                                {/*</div>*/}
-                                {/*<div className='is-login__title'>Login to Your Account</div>*/}
-                                {/*<TextFieldValidated*/}
-                                {/*    placeholder={intl.formatMessage({ id: "email" })}*/}
-                                {/*    validateRule={validateField.bind(this, 'email')}*/}
-                                {/*    formState={formState}*/}
-                                {/*    direction='-column'*/}
-                                {/*    errorAlign='login'*/}
-                                {/*    maxLength={50}*/}
-                                {/*    field="email"*/}
-                                {/*    type="email"*/}
-                                {/*/>*/}
-                                {/*<div className="is-login__password-wrap">*/}
-                                {/*    <TextFieldValidated*/}
-                                {/*        placeholder={intl.formatMessage({ id: "password" })}*/}
-                                {/*        validateRule={validateField.bind(this, 'password')}*/}
-                                {/*        additionalClass='passwordInput'*/}
-                                {/*        formState={formState}*/}
-                                {/*        direction='-column'*/}
-                                {/*        errorAlign='login'*/}
-                                {/*        field="password"*/}
-                                {/*        type="password"*/}
-                                {/*        maxLength={50}*/}
-                                {/*    />*/}
-                                {/*    {isActiveCapsLock && <Image title={intl.formatMessage({ id: "caps_lock_warning" })} src='/static/icons/ico_error.svg' className="is-login__caps-lock"/>}*/}
-                                {/*</div>*/}
-                                {/*<div className="is-login__button">*/}
-                                {/*    <CustomButton*/}
-                                {/*        content={intl.formatMessage({ id: "login_btn" })}*/}
-                                {/*        className='is-login__button-login'*/}
-                                {/*        disabled={isLoading}*/}
-                                {/*        loading={isLoading}*/}
-                                {/*        icon="login"*/}
-                                {/*        color='blue'*/}
-                                {/*        type="submit"*/}
-                                {/*    />*/}
-                                {/*</div>*/}
-                                {/*<div className="is-login__link">*/}
-                                {/*    <Link href="/account_recovery">*/}
-                                {/*        <div className={classNames('is-login__link-text', { disabled: isLoading })}>*/}
-                                {/*            {intl.formatMessage({ id: "forgotPassword" })}*/}
-                                {/*        </div>*/}
-                                {/*    </Link>*/}
-                               {/*</div>*/}
                     </Form>
         </div>
     );
